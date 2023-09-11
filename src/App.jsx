bc3b09275ae45ed5aeedabaef32261ccd01e83c7
Bruno <bruno.delmoro@modis.com>
2023-09-11 17:11:01 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import "./App.css";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Projects from "./components/Projects";
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="text-white">Hello</div>
+    </>
+  );
+}
+
+export default App;
