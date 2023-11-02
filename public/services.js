69ee1a5155dd42dda4cf5ca37dc077ea1be269c5
Bruno <delmo123123@gmail.com>
2023-11-02 18:15:06 +0100

    eslint rules

--- public/services.js
+++ public/services.js
@@ -4,13 +4,26 @@ import photo3 from './assets/icons/android.png';
 import photo4 from './assets/icons/git-repo.png';
 import photo5 from './assets/icons/javascript.png';
 import photo6 from './assets/icons/web-scraping.png';
+
 const services = [
-    {id: 1, name:'WEB DEVELOPMENT', description:'I create beautiful iterfaces with simple HTML, CSS, & JavaScript and also frameworks like ReactJS and NextJS', image: photo1},
-    {id:2, name:'BACK END', description:'Understanding of REST APIs, creating them through NodeJs, either using Express or NestJS, maintenance, best practices and high understanding of solutions', image: photo2},
-    {id:3, name:'MOBILE DEVELOPMENT', description:'Learning about mobile developer. Creating my own app using Flutter', image: photo3},
-    {id:4, name:'VERSION CONTROL', description:'I can use version control systems well, and Git & Bitbucket are my go-to tool, combining them with Scrum Mastering', image:photo4},
-    {id:5, name:'NPM AND NODEJS', description:'I have core understanding of NPM. I can also develop general purpose applications with NodeJS', image:photo5},
-    {id:6, name:'SOFT SKILLS', description:'I consider the soft skills a priority in terms of group management and coexistence, I am passionate about working in a team and learning within the team', image:photo6},
-]
+  {
+    id: 1, name: 'WEB DEVELOPMENT', description: 'I create beautiful iterfaces with simple HTML, CSS, & JavaScript and also frameworks like ReactJS and NextJS', image: photo1,
+  },
+  {
+    id: 2, name: 'BACK END', description: 'Understanding of REST APIs, creating them through NodeJs, either using Express or NestJS, maintenance, best practices and high understanding of solutions', image: photo2,
+  },
+  {
+    id: 3, name: 'MOBILE DEVELOPMENT', description: 'Learning about mobile developer. Creating my own app using Flutter', image: photo3,
+  },
+  {
+    id: 4, name: 'VERSION CONTROL', description: 'I can use version control systems well, and Git & Bitbucket are my go-to tool, combining them with Scrum Mastering', image: photo4,
+  },
+  {
+    id: 5, name: 'NPM AND NODEJS', description: 'I have core understanding of NPM. I can also develop general purpose applications with NodeJS', image: photo5,
+  },
+  {
+    id: 6, name: 'SOFT SKILLS', description: 'I consider the soft skills a priority in terms of group management and coexistence, I am passionate about working in a team and learning within the team', image: photo6,
+  },
+];
 
 export default services;
