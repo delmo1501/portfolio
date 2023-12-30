4b050daba457c74dab45a4a5718a13702d3ea756
Bruno <delmo123123@gmail.com>
2023-12-30 22:59:42 +0100

    Setting Dark/light mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import '@mui/material/styles';
+import { ThemeProvider, createTheme, useMediaQuery } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
 import Header from './components/Header';
 import Home from './components/Home';
 import About from './components/About';
@@ -9,15 +11,35 @@ import Contact from './components/Contact';
 import Footer from './components/Footer';
 
 function App() {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const [mode, setMode] = useState(prefersDarkMode ? 'dark' : 'light');
+
+  const theme = createTheme({
+    palette: {
+      mode,
+    },
+  });
+
+  const toggleMode = () => {
+    setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+  };
+
+  useEffect(() => {
+    setMode(prefersDarkMode ? 'dark' : 'light');
+  }, [prefersDarkMode]);
+
   return (
-    <div className="max-w-7xl mx-auto">
-      <Header />
-      <Home />
-      <About />
-      <Projects />
-      <Contact />
-      <Footer />
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="max-w-7xl mx-auto">
+        <Header toggleMode={toggleMode} />
+        <Home />
+        <About />
+        <Projects />
+        <Contact />
+        <Footer />
+      </div>
+    </ThemeProvider>
   );
 }
 
